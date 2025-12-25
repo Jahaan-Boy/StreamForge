ac4ed83d7ce638e5363adeacc80205ae6abc16d0
Jahaan-Boy <shahpravin863@gmail.com>
2025-12-25 18:37:35 +0530

    Final-part-1

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,20 +6,3 @@ const asyncHandler=(fn)=>{
 }
 
 export {asyncHandler};
-
-
-
-
-
-
-
-// const asyncHandler=(fn)=> async (req,res,next)=>{
-//     try{
-//         await fn(req,res,next);
-//     }catch(err){
-//         res.status(500).json({
-//             success:false,
-//             message:err.message
-//         })
-//     }
-// }
